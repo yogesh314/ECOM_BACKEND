338f9e18fa7fa50775ffc2b9b3c607271e797e4b
yogesh314 <yogeshatworld3.14@gmail.com>
2025-03-02 02:17:51 +0530

    3. Adding ResponseEntity for Http status.

--- src/main/java/com/yogesh/ECOM_BACKEND/controller/ProductController.java
+++ src/main/java/com/yogesh/ECOM_BACKEND/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.yogesh.ECOM_BACKEND.controller;
 import com.yogesh.ECOM_BACKEND.model.Product;
 import com.yogesh.ECOM_BACKEND.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProduct(){
-        return productService.getAllProducts() ;
+    public ResponseEntity<List<Product>> getProduct(){
+        //This ResponseEntity is used to send different Http status to client
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 }
